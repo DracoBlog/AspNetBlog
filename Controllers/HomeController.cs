dd84b36ef765a37450d6984cd8418771d1b6ffbf
alexand1r <s123189@gmail.com>
2016-08-04 18:46:32 +0300

    Added Post PostController with CRUD / Authorizations for admin and current user

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Blog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var db = new ApplicationDbContext();
+            var post = db.Posts.OrderByDescending(p => p.Date);
+            return View(post.ToList());
+        }
+
+    }
+}
